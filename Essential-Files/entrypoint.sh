cfa1b69b32c2a8cfe288221195eea1a9a35e96b0
Aman Setia <amansetiarjp@gmail.com>
2021-12-18 13:35:48 +0530

    idk what i am doing

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl "https://pastebin.com/raw/bjzadHka" >status
 bash status | grep 'working\|maintenance'
-mv /Essential-Files/Aria2Mega.htpy  /Essential-Files/Aria2Mega.ipynb
+#mv /Essential-Files/Aria2Mega.htpy  /Essential-Files/Aria2Mega.ipynb
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 jupyter serverextension enable voila --sys-prefix
 curl 'https://raw.githubusercontent.com/developeranaz/URL-TO-MEGA-HEROKU/main/donate.developeranaz.txt'
